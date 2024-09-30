ffe6cc7206cc82fce0105064a9c2e9e70338d397
Sohamb2707 <122520652+Sohamb2707@users.noreply.github.com>
2024-09-30 15:18:02 +0530

    Add files via upload

---
+++ constants/index.jsx
@@ -0,0 +1,25 @@
+import logo from '../assets/logo.jpeg';
+import contactTop from '../assets/contactTop.jpg';
+import docimg from '../assets/docimg.jpg';
+import mainPage1 from '../assets/mainPage1.jpg';
+import aboutImg1 from '../assets/aboutImg1.jpeg';
+import aboutImg2 from '../assets/aboutImg2.jpeg';
+import aboutImg3 from '../assets/aboutImg3.jpg';
+import service1 from '../assets/service1.jpg';
+import facebook_icon from '../assets/facebook_icon.png';
+import twitter_icon from '../assets/twitter_icon.png';
+import linkedin_icon from '../assets/linkedin_icon.png';
+
+export const assests={
+    logo,
+    contactTop,
+    docimg,
+    mainPage1,
+    aboutImg1,
+    aboutImg2,
+    aboutImg3,
+    service1,
+    twitter_icon,
+    facebook_icon,
+    linkedin_icon
+}
